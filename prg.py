5c6c9444bd24a95067c0ae82bfe7a413e689eb6b
Apurbo <tanvirmahfuz22@gmail.com>
2025-09-04 12:08:00 +0600

    first commit

---
+++ prg.py
@@ -0,0 +1,14 @@
+import requests
+
+# Public GitHub repo (Torvalds Linux as example)
+url = "https://api.github.com/repos/torvalds/linux/commits"
+
+# Fetch commits
+response = requests.get(url)
+commits = response.json()
+# print(commits[0]['commit']['author'])
+
+for commit in commits:
+    author = commit["commit"]["author"]["name"]
+    message = commit["commit"]["message"]
+    print(f"{author}: {message}\n")
